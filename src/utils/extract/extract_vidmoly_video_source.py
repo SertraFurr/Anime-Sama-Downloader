c0889df1a55c1da397d07d83f0453996189b05aa
Sertra <Amaryllisaea@gmail.com>
2026-01-10 19:51:12 +0100

    Refactor video source extraction functions

--- src/utils/extract/extract_vidmoly_video_source.py
+++ src/utils/extract/extract_vidmoly_video_source.py
@@ -1,9 +1,30 @@
 import re
+from src.utils.fetch.fetch_page_content import fetch_page_content
 from bs4 import BeautifulSoup
-
 from src.var import print_status
 
-def extract_vidmoly_video_source(html_content):
+    
+def extract_vidmoly_video_hash_source(html_content):
+    if not html_content:
+        return None
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    for script in soup.find_all('script'):
+        if script.string and '?g=' in script.string:
+            match = re.search(r'\?g=([a-f0-9]{32})', script.string)
+            if match:
+                return match.group(1)
+
+    return None
+
+def extract_vidmoly_video_source(html_content, page_url):
+    hash = extract_vidmoly_video_hash_source(html_content)
+
+    request_url = f"{page_url}?g={hash}"
+
+    html_content = fetch_page_content(request_url)
+
     if not html_content:
         return None
     soup = BeautifulSoup(html_content, 'html.parser')
