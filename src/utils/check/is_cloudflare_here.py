b4643b575b13ad41f4815c853b79018993b815c9
Sertra <Amaryllisaea@gmail.com>
2026-01-11 13:28:28 +0100

    Update is_cloudflare_here.py

--- src/utils/check/is_cloudflare_here.py
+++ src/utils/check/is_cloudflare_here.py
@@ -3,7 +3,6 @@ import requests
 def check_if_cloudflare_enabled(domain, headers):
     try:
         response = requests.get(f"https://{domain}/", headers=headers, timeout=10)
-        print(response.text)
         if "https://raw.githubusercontent.com/Anime-Sama/IMG/img/contenu/orb-on-the-movements-of-the-earth.jpg" not in response.text:
             return True
         return False
