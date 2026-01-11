6ecd44da9741e0347fc744fabbd125e6a4c7292e
Sertra <Amaryllisaea@gmail.com>
2026-01-11 21:41:07 +0100

    Update validate_anime_sama_url.py

--- src/utils/validate_anime_sama_url.py
+++ src/utils/validate_anime_sama_url.py
@@ -3,7 +3,7 @@ from src.var import Colors, get_domain
 
 def validate_anime_sama_url(url):
     pattern = re.compile(
-    r'^https?://(?:www\.)?anime-sama\.[^/]+/catalogue/[^/]+/(?:saison\d+(?:-\d+)?|film\d*)/(?:vostfr|vo|vf\d*)/?$', 
+    r'^https?://(?:www\.)?anime-sama\.[^/]+/catalogue/[^/]+/(?:saison\d+(?:-\d+)?|film\d*|scan)/(?:vostfr|vo|vf\d*)/?$', 
     re.IGNORECASE
     )
     if pattern.match(url):
@@ -12,5 +12,6 @@ def validate_anime_sama_url(url):
         return False, (
             f"{url} Invalid URL. Format should be:\n"
             f"  https://{get_domain()}/catalogue/<anime-name>/saison<NUMBER>/<language>/\n"
+            f"  https://{get_domain()}/catalogue/<anime-name>/scan/<language>/\n"
             "Where <language> is VOSTFR, VF, VO, etc. Also .org domain is accepted."
         )
