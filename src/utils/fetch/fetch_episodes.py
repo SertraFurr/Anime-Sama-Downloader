c3129856a59b762e8ffd556c2d721fb7ebcfd4ce
Sertra <Amaryllisaea@gmail.com>
2026-01-11 03:47:34 +0100

    Update fetch_episodes.py

--- src/utils/fetch/fetch_episodes.py
+++ src/utils/fetch/fetch_episodes.py
@@ -1,20 +1,8 @@
 import re
 import requests
 from src.var import print_status
-from src.utils.config.config import get_cookies
 
-headers = {
-    "Accept": "text/html",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "fr-FR,fr;q=0.8",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-    "Cookie": "cf_clearance=" + get_cookies()
-}
-
-def fetch_episodes(base_url):
+def fetch_episodes(base_url, headers=None):
     js_url = base_url.rstrip('/') + '/episodes.js'
     print_status("Fetching episode list...", "loading")
     try:
