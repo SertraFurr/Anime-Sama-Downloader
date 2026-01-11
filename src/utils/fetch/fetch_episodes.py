0778600ce98e0d57f5da42dbfbbc011c55e1be0b
Sertra <Amaryllisaea@gmail.com>
2026-01-11 03:37:58 +0100

    Add headers and cookie handling to fetch_episodes

--- src/utils/fetch/fetch_episodes.py
+++ src/utils/fetch/fetch_episodes.py
@@ -1,14 +1,24 @@
 import re
 import requests
-
 from src.var import print_status
+from src.utils.config.config import get_cookies
+
+headers = {
+    "Accept": "text/html",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "fr-FR,fr;q=0.8",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+    "Cookie": "cf_clearance=" + get_cookies()
+}
 
 def fetch_episodes(base_url):
     js_url = base_url.rstrip('/') + '/episodes.js'
     print_status("Fetching episode list...", "loading")
-    
     try:
-        response = requests.get(js_url, timeout=15)
+        response = requests.get(js_url, headers=headers, timeout=15)
         response.raise_for_status()
         js_content = response.text
     except Exception as e:
