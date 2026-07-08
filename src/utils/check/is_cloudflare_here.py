21cd5f47108b2617d9aee0db18acc867c262ef47
Sertra <196074761+SertraFurr@users.noreply.github.com>
2026-07-08 18:27:58 +0200

    Update Cloudflare check condition in is_cloudflare_here.py

--- src/utils/check/is_cloudflare_here.py
+++ src/utils/check/is_cloudflare_here.py
@@ -3,7 +3,7 @@ import requests
 def check_if_cloudflare_enabled(domain, headers):
     try:
         response = requests.get(f"https://{domain}/", headers=headers, timeout=10)
-        if "https://raw.githubusercontent.com/Anime-Sama/IMG/img/contenu/orb-on-the-movements-of-the-earth.jpg" not in response.text:
+        if "/catalogue" not in response.text:
             return True
         return False
     except requests.RequestException:
