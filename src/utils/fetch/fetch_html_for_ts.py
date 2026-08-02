8b49da9d3259eb857e2a8fc889225c0da6d65c64
Sertra <196074761+SertraFurr@users.noreply.github.com>
2026-08-02 20:18:33 +0200

    Update fetch_html_for_ts.py

--- src/utils/fetch/fetch_html_for_ts.py
+++ src/utils/fetch/fetch_html_for_ts.py
@@ -6,7 +6,7 @@ def fetch_html_for_ts(url):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Language': 'en-US,en;q=0.9',
-            'Referer': url.split('/embed/')[0],
+            'Referer': f"{url.split('//')[0]}//{url.split('//')[1].split('/')[0]}/",
             'Connection': 'keep-alive',
         }
         response = requests.get(url, headers=headers, timeout=10)
