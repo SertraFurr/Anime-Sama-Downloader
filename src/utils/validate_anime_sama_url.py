f8c2d9370487607986a0c50819fc8ef310c6ede6
Sertra <196074761+SertraFurr@users.noreply.github.com>
2026-08-02 20:16:09 +0200

    Update validate_anime_sama_url.py

--- src/utils/validate_anime_sama_url.py
+++ src/utils/validate_anime_sama_url.py
@@ -1,16 +1,21 @@
 import re
+import urllib.parse
 from src.var import Colors, get_domain
 
 def validate_anime_sama_url(url):
-    pattern = re.compile(
-    r'^https?://(?:www\.)?anime-sama\.[^/]+/catalogue/[^/]+/.+/.+/?$', 
-    re.IGNORECASE
+    anime_sama_pattern = re.compile(
+        r'^https?://(?:www\.)?anime-sama\.[^/]+/catalogue/[^/]+/.+/.+/?$', 
+        re.IGNORECASE
     )
-    if pattern.match(url):
+    nakanime_pattern = re.compile(
+        r'^https?://(?:www\.)?nakanime\.tv/(?:anime/\d+|catalog\?.*overlay=).*$',
+        re.IGNORECASE
+    )
+    if anime_sama_pattern.match(url) or nakanime_pattern.match(url):
         return True, ""
     else:
         return False, (
             f"{url} Invalid URL. Format should be:\n"
             f"  https://{get_domain()}/catalogue/<anime-name>/<season-type>/<language>/\n"
-            f"  https://{get_domain()}/catalogue/<anime-name>/scan/<language>/\n"
+            f"  https://nakanime.tv/anime/<id>/season/<s_num>/episode/<ep_num>\n"
         )
